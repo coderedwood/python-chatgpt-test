6215c6d71bc33a2901a088e97d8535c83e85ab20
Dwayne Redwood <87104481+coderedwood@users.noreply.github.com>
2023-03-15 22:39:44 -0500

    added test python file with basic code

---
+++ pygpt_test.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import os
+import openai
+
+load_dotenv()
+
+openai.api_key = os.getenv("API_KEY")
+
+prompt = "Say this is a test"
+
+response = openai.Completion.create(
+    model="text-davinci-003", prompt=prompt, temperature=0, max_tokens=7)
+print(response)
